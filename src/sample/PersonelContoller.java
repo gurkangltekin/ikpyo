8eba4694a9006a9fd87bcae9e087b7439edab370
gurkangltekin <gurkangltrekin@icloud.com>
2020-04-09 21:12:43 +0300

    Initial commit

---
+++ src/sample/PersonelContoller.java
@@ -0,0 +1,14 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class PersonelContoller {
+    public TextField a;
+
+    public void hesapla(ActionEvent e){
+        a.setText("deneme");
+    }
+
+}
