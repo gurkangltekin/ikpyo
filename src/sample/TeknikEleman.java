648919f33977e0325d5651e5c798ca08c8c932fd
Suzan Nur <suzanbulbl@gmail.com>
2020-04-16 13:27:29 +0300

    deneme

--- src/sample/TeknikEleman.java
+++ src/sample/TeknikEleman.java
@@ -1,4 +1,14 @@
 package sample;
 
 public class TeknikEleman {
+    private int tecrube;
+
+
+    public int getTecrube() {
+        return tecrube;
+    }
+
+    public void setTecrube(int tecrube) {
+        this.tecrube = tecrube;
+    }
 }
