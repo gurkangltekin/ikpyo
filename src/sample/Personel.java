d00f06be52877cdf134e9a1f03a27a49f2085340
gurkangltekin <gurkangltrekin@icloud.com>
2020-04-16 12:05:05 +0300

    Initial commit

--- src/sample/Personel.java
+++ src/sample/Personel.java
@@ -1,4 +1,108 @@
 package sample;
 
+import java.sql.Timestamp;
+import java.util.*;
+
 public class Personel {
+
+    private String adi;
+    private String soyadi;
+    private Long TCKimlik;
+    private Date iseGiris;
+    private String departman;
+    private int maas;
+    private List<Timestamp> girisSaati;
+    private List<Timestamp> cikisSaati;
+
+    public void maasGuncelle(int maas){
+        this.setMaas(maas);
+    }
+
+    public void personelGirdi(Timestamp girisSaati){
+        this.getGirisSaati().add(girisSaati);
+    }
+
+    public void personelCikti(Timestamp cikisSaati){
+        this.getCikisSaati().add(cikisSaati);
+    }
+
+    public String getAdi() {
+        if(this.adi == null)
+            this.adi = "Adı Belirtilmemiş!";
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+
+    public String getSoyadi() {
+        if(this.soyadi == null)
+            this.soyadi = "Soyadı Belirtilmemiş!";
+        return soyadi;
+    }
+
+    public void setSoyadi(String soyadi) {
+        this.soyadi = soyadi;
+    }
+
+    public Long getTCKimlik() {
+        if(this.TCKimlik == 0)
+            this.TCKimlik = 1000000000L;
+        return TCKimlik;
+    }
+
+    public void setTCKimlik(Long TCKimlik) {
+        this.TCKimlik = TCKimlik;
+    }
+
+    public Date getIseGiris() {
+        if(this.iseGiris == null)
+            this.iseGiris = new Date();
+        return iseGiris;
+    }
+
+    public void setIseGiris(Date iseGiris) {
+        this.iseGiris = iseGiris;
+    }
+
+    public String getDepartman() {
+        if(this.departman == null)
+            this.departman = "Departman Belirtilmemiş!";
+        return departman;
+    }
+
+    public void setDepartman(String departman) {
+        this.departman = departman;
+    }
+
+    public int getMaas() {
+        if(this.maas == 0)
+            this.maas = 2350;
+        return maas;
+    }
+
+    public void setMaas(int maas) {
+        this.maas = maas;
+    }
+
+    public List<Timestamp> getGirisSaati() {
+        if(this.girisSaati == null)
+            this.girisSaati = new ArrayList<Timestamp>();
+        return girisSaati;
+    }
+
+    public void setGirisSaati(List<Timestamp> girisSaati) {
+        this.girisSaati = girisSaati;
+    }
+
+    public List<Timestamp> getCikisSaati() {
+        if(this.cikisSaati == null)
+            this.cikisSaati = new ArrayList<Timestamp>();
+        return cikisSaati;
+    }
+
+    public void setCikisSaati(List<Timestamp> cikisSaati) {
+        this.cikisSaati = cikisSaati;
+    }
 }
--- src/sample/PersonelContoller.java
+++ src/sample/PersonelContoller.java
@@ -1,14 +1,183 @@
 package sample;
 
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-public class PersonelContoller {
-    public TextField a;
+import javax.swing.text.html.ImageView;
+import java.util.List;
 
-    public void hesapla(ActionEvent e){
-        a.setText("deneme");
+public class PersonelContoller {
+    private Personel personel;
+    private List<Personel> personelList;
+    private DosyaYazma dosyaYazOku;
+    @FXML
+    private TextField tcKimlikNo;
+    @FXML
+    private TextField adi;
+    @FXML
+    private TextField soyadi;
+    @FXML
+    private DatePicker iseGirisTarihi;
+    @FXML
+    private TextField maas;
+    @FXML
+    private TextField departman;
+    @FXML
+    private ListView<String> iseGirisSaatleri;
+    @FXML
+    private ListView<String> istenCikisSaatleri;
+    @FXML
+    private ListView<String> personelListesi = new ListView<String>();
+    @FXML
+    private Button kisiyiGuncelle_yeniKisiEkle;
+    @FXML
+    private Button istenCikar;
+    @FXML
+    private Button formuTemizle_Detay;
+    @FXML
+    private ImageView fotograf;
+
+    public void listele(ActionEvent e){
+        tcKimlikNo.setText("1000000000");
+        adi.setText("Gurkan");
+        soyadi.setText("Gultekin");
+        maas.setText("2000");
+        departman.setText("Bilişim");
+        this.personelListesi.setItems(FXCollections.observableArrayList("ADASDA","ADASDASD"));
+
     }
 
+    public Personel getPersonel() {
+        return personel;
+    }
+
+    public void setPersonel(Personel personel) {
+        this.personel = personel;
+    }
+
+    public List<Personel> getPersonelList() {
+        return personelList;
+    }
+
+    public void setPersonelList(List<Personel> personelList) {
+        this.personelList = personelList;
+    }
+
+    public DosyaYazma getDosyaYazOku() {
+        return dosyaYazOku;
+    }
+
+    public void setDosyaYazOku(DosyaYazma dosyaYazOku) {
+        this.dosyaYazOku = dosyaYazOku;
+    }
+
+    public TextField getTcKimlikNo() {
+        return tcKimlikNo;
+    }
+
+    public void setTcKimlikNo(TextField tcKimlikNo) {
+        this.tcKimlikNo = tcKimlikNo;
+    }
+
+    public TextField getAdi() {
+        return adi;
+    }
+
+    public void setAdi(TextField adi) {
+        this.adi = adi;
+    }
+
+    public TextField getSoyadi() {
+        return soyadi;
+    }
+
+    public void setSoyadi(TextField soyadi) {
+        this.soyadi = soyadi;
+    }
+
+    public DatePicker getIseGirisTarihi() {
+        return iseGirisTarihi;
+    }
+
+    public void setIseGirisTarihi(DatePicker iseGirisTarihi) {
+        this.iseGirisTarihi = iseGirisTarihi;
+    }
+
+    public TextField getMaas() {
+        return maas;
+    }
+
+    public void setMaas(TextField maas) {
+        this.maas = maas;
+    }
+
+    public TextField getDepartman() {
+        return departman;
+    }
+
+    public void setDepartman(TextField departman) {
+        this.departman = departman;
+    }
+
+    public ListView<String> getIseGirisSaatleri() {
+        return iseGirisSaatleri;
+    }
+
+    public void setIseGirisSaatleri(ListView<String> iseGirisSaatleri) {
+        this.iseGirisSaatleri = iseGirisSaatleri;
+    }
+
+    public ListView<String> getIstenCikisSaatleri() {
+        return istenCikisSaatleri;
+    }
+
+    public void setIstenCikisSaatleri(ListView<String> istenCikisSaatleri) {
+        this.istenCikisSaatleri = istenCikisSaatleri;
+    }
+
+    public ListView<String> getPersonelListesi() {
+        return personelListesi;
+    }
+
+    public void setPersonelListesi(ListView<String> personelListesi) {
+        this.personelListesi = personelListesi;
+    }
+
+    public Button getKisiyiGuncelle_yeniKisiEkle() {
+        return kisiyiGuncelle_yeniKisiEkle;
+    }
+
+    public void setKisiyiGuncelle_yeniKisiEkle(Button kisiyiGuncelle_yeniKisiEkle) {
+        this.kisiyiGuncelle_yeniKisiEkle = kisiyiGuncelle_yeniKisiEkle;
+    }
+
+    public Button getIstenCikar() {
+        return istenCikar;
+    }
+
+    public void setIstenCikar(Button istenCikar) {
+        this.istenCikar = istenCikar;
+    }
+
+    public Button getFormuTemizle_Detay() {
+        return formuTemizle_Detay;
+    }
+
+    public void setFormuTemizle_Detay(Button formuTemizle_Detay) {
+        this.formuTemizle_Detay = formuTemizle_Detay;
+    }
+
+    public ImageView getFotograf() {
+        return fotograf;
+    }
+
+    public void setFotograf(ImageView fotograf) {
+        this.fotograf = fotograf;
+    }
 }
